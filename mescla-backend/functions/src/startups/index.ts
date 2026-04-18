a34112db179062aa001f0866d9c29f6a5e18e97f
mateus marinho <matiazzsouza@gmail.com>
2026-04-18 11:46:45 -0300

    adicionando arquivos

--- mescla-backend/functions/src/startups/index.ts
+++ mescla-backend/functions/src/startups/index.ts
@@ -2,3 +2,4 @@ export {createStartupQuestion} from "./handlers/createStartupQuestion";
 export {getStartupDetails} from "./handlers/getStartupsDetails";
 export {listStartups} from "./handlers/listStartups";
 export {seedStartupCatalog} from "./handlers/seedStartupCatalog";   
+export {addStartup} from "./handlers/addStartup";
