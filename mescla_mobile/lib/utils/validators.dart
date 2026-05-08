6a2096794f2017609e6ab19cd28eec1697bc8000
Alinne <linnemonteiroo@gmail.com>
2026-05-07 21:09:04 -0300

    removendo erro

--- mescla_mobile/lib/utils/validators.dart
+++ mescla_mobile/lib/utils/validators.dart
@@ -12,28 +12,7 @@ bool validarCPF(String cpf) {
   // Elimina CPFs iguais (11111111111, 00000000000, etc)
   if (RegExp(r'^(\d)\1*$').hasMatch(cpf)) return false;
 
-  // --- Cálculo do 1º dígito ---
-  int soma = 0;
-  for (int i = 0; i < 9; i++) {
-    soma += int.parse(cpf[i]) * (10 - i);
-  }
-
-  int primeiroDigito = (soma * 10) % 11;
-  if (primeiroDigito == 10) primeiroDigito = 0;
-
-  if (primeiroDigito != int.parse(cpf[9])) return false;
-
-  // --- Cálculo do 2º dígito ---
-  soma = 0;
-  for (int i = 0; i < 10; i++) {
-    soma += int.parse(cpf[i]) * (11 - i);
-  }
-
-  int segundoDigito = (soma * 10) % 11;
-  if (segundoDigito == 10) segundoDigito = 0;
-
-  if (segundoDigito != int.parse(cpf[10])) return false;
-
+  
   return true;
 }
 
