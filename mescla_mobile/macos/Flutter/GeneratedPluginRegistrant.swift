762383792bc230dcc21f9225afbf34b24503a3df
Bruna Barbour Fernandes <bbfhernandez@gmail.com>
2026-05-11 15:20:43 -0300

    detalhes de integracao

--- mescla_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mescla_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import cloud_firestore
 import cloud_functions
 import firebase_auth
 import firebase_core
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FirebaseFunctionsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  VideoPlayerPlugin.register(with: registry.registrar(forPlugin: "VideoPlayerPlugin"))
 }
