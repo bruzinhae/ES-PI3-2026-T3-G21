07bb5b7b309e740c7790e883fc897119a6390b58
Padreeca <g.padrecanicoletti@gmail.com>
2026-05-12 10:50:18 -0300

    feat: add simulated token purchase flow

--- mescla-backend/functions/src/wallet/index.ts
+++ mescla-backend/functions/src/wallet/index.ts
@@ -1,4 +1,6 @@
-export { listUserAssets } from "./handlers/listUserAssets";
-export { getUserAssetByStartup } from "./handlers/getUserAssetByStartup";
-export { getUserWallet } from "./handlers/getUserWallet";
-export { depositToUserWallet } from "./handlers/depositToUserWallet";
+export {listUserAssets} from "./handlers/listUserAssets";
+export {getUserAssetByStartup} from "./handlers/getUserAssetByStartup";
+export {getUserWallet} from "./handlers/getUserWallet";
+export {depositToUserWallet} from "./handlers/depositToUserWallet";
+export {buyTokens} from "./handlers/buyTokens";
+export {listUserTransactions} from "./handlers/listUserTransactions";
